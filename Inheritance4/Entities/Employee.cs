7b0661cb164f6a5b098b05a07e1430efd4f289d3
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 12:48:41 -0300

    Create Employee Entity

---
+++ Inheritance4/Entities/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Globalization;
+namespace Inheritance4.Entities
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public int Hours { get; private set; }
+        public double ValuePerHour { get; private set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            double payment = Hours * ValuePerHour;
+            return payment;
+
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append("- $");
+            sb.Append(Payment().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+
+        }
+    }
+}
