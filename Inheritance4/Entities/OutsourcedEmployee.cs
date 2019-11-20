e1135723db566f059a8d257b3f0289e69d599cac
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 12:49:30 -0300

    Create OutsourceEmployee Entity

---
+++ Inheritance4/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance4.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double aditionalCharge) : base(name, hours, valuePerHour)
+        {
+            AditionalCharge = aditionalCharge;
+        }
+
+        public override double Payment()
+        {
+            double payment = base.Payment() + AditionalCharge * 1.10;
+            return payment;
+        }
+    }
+}
